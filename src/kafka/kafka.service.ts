44a9f376047282be56668c02bb052e0b70bc2eb3
chanyeong <jcy030896@naver.com>
2024-08-18 20:54:02 +0900

    fix: commit 실패시 재시도 로직 적용

--- src/kafka/kafka.service.ts
+++ src/kafka/kafka.service.ts
@@ -1,4 +1,10 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  BadGatewayException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Kafka, EachMessagePayload, Consumer } from 'kafkajs';
 import { PuppeteerService } from '../puppeteer/puppeteer.service';
 import { MinioService } from '../minio/minio.service';
@@ -26,35 +32,38 @@ export class KafkaService implements OnModuleInit, OnModuleDestroy {
     await this.consumer.connect();
     await this.consumer.subscribe({ topic: this.topic, fromBeginning: false });
 
-    await this.consumer.run({
-      autoCommit: false,
-      eachMessage: async (payload: EachMessagePayload) => {
-        const { topic, partition, message } = payload;
-        console.info(
-          `partition: ${partition}, messageOffset: ${message.offset}`,
-        );
-        const uuid = message.key.toString();
-        const htmlContent = message.value.toString();
-        try {
-          const pdf = await this.puppeteerService.generatePDF(htmlContent);
-          await this.minioService.uploadPdf(
-            uuid,
-            Buffer.from(pdf),
-            message.headers,
+    await this.consumer
+      .run({
+        autoCommit: false,
+        eachMessage: async (payload: EachMessagePayload) => {
+          const { topic, partition, message } = payload;
+          console.info(
+            `partition: ${partition}, messageOffset: ${message.offset}`,
           );
-          // 성공 시 커밋
-          await this.consumer.commitOffsets([
-            {
-              topic,
-              partition,
-              offset: (BigInt(message.offset) + BigInt(1)).toString(),
-            },
-          ]);
-        } catch (e) {
-          console.log(e);
-        }
-      },
-    });
+          const uuid = message.key.toString();
+          const htmlContent = message.value.toString();
+          try {
+            const pdf = await this.puppeteerService.generatePDF(htmlContent);
+            await this.minioService.uploadPdf(
+              uuid,
+              Buffer.from(pdf),
+              message.headers,
+            );
+            // 성공 시 커밋
+            // throw new BadGatewayException('일부러 에러 발생');
+            await this.consumer.commitOffsets([
+              {
+                topic,
+                partition,
+                offset: (BigInt(message.offset) + BigInt(1)).toString(),
+              },
+            ]);
+          } catch (e) {
+            throw new InternalServerErrorException(`[consumer] ${e.message}`);
+          }
+        },
+      })
+      .catch((e) => console.error(`[example/consumer] ${e.message}`, e));
   }
 
   async onModuleDestroy() {
